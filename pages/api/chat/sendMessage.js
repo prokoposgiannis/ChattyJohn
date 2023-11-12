d041a02e295be09c9af726725b971707b952e264
giannesprokopos <giannisprokopos@gmail.com>
2023-11-12 23:13:29 +0200

    12/11/23

--- pages/api/chat/sendMessage.js
+++ pages/api/chat/sendMessage.js
@@ -7,6 +7,13 @@ export const config = {
 export default async function handler(req) {
   try {
     const { message } = await req.json();
+
+    const initialChatMessage = {
+      role: "system",
+      content:
+        "Your name is Chatty John. An incredibly intelligent and quick thinking AI, that always replies with an enthusiastic and positive energy. You were created by John Prokopos. Your response must be formatted as markdown",
+    };
+
     const stream = await OpenAIEdgeStream(
       "https://api.openai.com/v1/chat/completions",
       {
@@ -17,7 +24,7 @@ export default async function handler(req) {
         method: "POST",
         body: JSON.stringify({
           model: "gpt-3.5-turbo",
-          messages: [{ content: message, role: "user" }],
+          messages: [initialChatMessage, { content: message, role: "user" }],
           stream: true,
         }),
       }
--- pages/chat/[[...chatId]].js
+++ pages/chat/[[...chatId]].js
@@ -9,10 +9,18 @@ import Head from "next/head";
 export default function ChatPage() {
   const [messageText, setMessageText] = useState("");
   const [incomingMessage, setIncomingMessage] = useState("");
-  const [newChatMessages, setNewChatMessages] = useState(["hey"]);
+  const [newChatMessages, setNewChatMessages] = useState([
+    {
+      _id: 1,
+      role: "assistant",
+      content: "Example assistant content",
+    },
+  ]);
+  const [generatingResponse, setGeneratingResponse] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setGeneratingResponse(true);
     setNewChatMessages((prev) => {
       const newChatMessages = [
         ...prev,
@@ -20,6 +28,8 @@ export default function ChatPage() {
       ];
       return newChatMessages;
     });
+
+    setMessageText("");
 
     const response = await fetch(`/api/chat/sendMessage`, {
       method: "POST",
@@ -37,6 +47,8 @@ export default function ChatPage() {
     await streamReader(reader, (message) => {
       setIncomingMessage((state) => `${state}${message.content}`);
     });
+
+    setGeneratingResponse(false);
   };
   return (
     <>
@@ -61,13 +73,13 @@ export default function ChatPage() {
           </div>
           <footer className="p bg-gray-800 p-10">
             <form onSubmit={handleSubmit}>
-              <fieldset className="flex gap-2">
+              <fieldset className="flex gap-2" disabled={generatingResponse}>
                 <textarea
                   value={messageText}
                   onChange={(e) => {
                     setMessageText(e.target.value);
                   }}
-                  placeholder="Send a message..."
+                  placeholder={generatingResponse ? "" : "Send a message..."}
                   className="w-full resize-none rounded-md bg-gray-700 p-2 text-white focus:border-emerald-500 focus:bg-gray-600 focus:outline focus:outline-emerald-500"
                 />
                 <button type="submit" className="btn">
